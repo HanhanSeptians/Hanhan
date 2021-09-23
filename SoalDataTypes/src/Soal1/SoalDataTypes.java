7cc7c6ac06a712ac71033cb33d7afedaa490b048
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2021-09-24 02:16:48 +0700

    Perbaikan

--- SoalDataTypes/src/Soal1/SoalDataTypes.java
+++ SoalDataTypes/src/Soal1/SoalDataTypes.java
@@ -9,17 +9,17 @@ public class SoalDataTypes {
 	        batas = keyboard.nextInt();
 	        
 	        for (indeks=0; indeks<batas; indeks++){
-	            System.out.printf("Masukkan Nilai yang akan di cek :");
-	            nilai = keyboard.nextInt();
-	            System.out.println(nilai+" Can be fitted in");
-	            try {
+	            try{
+		            System.out.printf("Masukkan Nilai yang akan di cek :");
+		            nilai = keyboard.nextInt();
+		            System.out.println(nilai+" Can be fitted in");
 		            if(nilai>=Byte.MIN_VALUE && nilai<= Byte.MAX_VALUE) System.out.println("* byte");
 		            if(nilai>=Short.MIN_VALUE && nilai<= Short.MAX_VALUE) System.out.println("* short");
 		            if(nilai>=Integer.MIN_VALUE && nilai<= Integer.MAX_VALUE) System.out.println("* integer");
 		            if(nilai>=Long.MIN_VALUE && nilai<= Long.MAX_VALUE) System.out.println("* long");
 	            }
-	            catch (Exception e) {
-	                System.out.println(nilai+" Can't be fitted anywhere");
+	            catch(Exception e) {
+	                System.out.println(" Can't be fitted anywhere.");
 	            }
 	       }
 	   }
