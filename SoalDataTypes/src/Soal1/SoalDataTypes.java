968b03f05618715c273f39f2582a1feebfac4b73
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2021-09-24 01:43:53 +0700

    Soal 1 Data types

---
+++ SoalDataTypes/src/Soal1/SoalDataTypes.java
@@ -0,0 +1,26 @@
+package Soal1;
+import java.util.*;
+
+public class SoalDataTypes {
+	    public static void main(String[] args) {
+	    int batas, nilai, indeks;	
+	        Scanner keyboard = new Scanner(System.in);
+	        System.out.printf("Masukkan jumlah nilai yang akan di cek : ");
+	        batas = keyboard.nextInt();
+	        
+	        for (indeks=0; indeks<batas; indeks++){
+	            System.out.printf("Masukkan Nilai yang akan di cek :");
+	            nilai = keyboard.nextInt();
+	            System.out.println(nilai+" Can be fitted in");
+	            try {
+		            if(nilai>=Byte.MIN_VALUE && nilai<= Byte.MAX_VALUE) System.out.println("* byte");
+		            if(nilai>=Short.MIN_VALUE && nilai<= Short.MAX_VALUE) System.out.println("* short");
+		            if(nilai>=Integer.MIN_VALUE && nilai<= Integer.MAX_VALUE) System.out.println("* integer");
+		            if(nilai>=Long.MIN_VALUE && nilai<= Long.MAX_VALUE) System.out.println("* long");
+	            }
+	            catch (Exception e) {
+	                System.out.println(nilai+" Can't be fitted anywhere");
+	            }
+	       }
+	   }
+}
